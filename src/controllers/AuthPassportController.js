df8600125c71b5382e82271541989d6ad9a49b33
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-07-19 15:26:09 +0200

    feature(social login)
    - add facebook login
    - add google login
    - add twitter login

---
+++ src/controllers/AuthPassportController.js
@@ -0,0 +1,72 @@
+import dotenv from 'dotenv';
+import status from '../config/status';
+import { User } from '../queries';
+import * as helper from '../helpers';
+
+dotenv.config();
+
+/**
+ * A class to handle users authentication using social media platform
+ */
+export default class AuthPassportController {
+  /**
+   * @param {object} profile social media user information
+   * @returns {object} a user object
+   */
+  static getSocialMediaUser(profile = {}) {
+    const user = {};
+    if (profile.displayName) {
+      const [firstName, lastName] = profile.displayName.split(' ');
+      user.firstName = firstName;
+      user.lastName = lastName;
+    }
+    if (profile.name) {
+      user.firstName = profile.name.givenName;
+      user.lastName = profile.name.familyName;
+    }
+    if (profile.emails) {
+      user.email = profile.emails[0].value;
+    }
+    if (profile.username) {
+      user.username = profile.username;
+    }
+    return Object.keys(profile).length
+      ? {
+        ...user,
+        image: profile.photos[0].value,
+        accountProvider: profile.provider,
+        accountProviderUserId: profile.id
+      }
+      : {};
+  }
+
+  /**
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} an object containing user information
+   */
+  static async loginOrSignup(req, res) {
+    const user = req.user || req.body || {};
+    if (!Object.keys(user).length) {
+      return res.status(status.BAD_REQUEST).json({ errors: { body: 'should not be empty' } });
+    }
+    const newOrExistingUser = await User.findOrCreate(
+      { accountProvider: user.provider, accountProviderUserId: user.id },
+      AuthPassportController.getSocialMediaUser(user)
+    );
+
+    if (newOrExistingUser.errors) {
+      const errors = helper.checkCreateUpdateUserErrors(newOrExistingUser.errors);
+      const statusCode = errors.code;
+      delete errors.code;
+      return res.status(statusCode).json(errors);
+    }
+
+    res.redirect(
+      302,
+      `${process.env.APP_URL_FRONTEND}/auth/${helper.token.generate({
+        id: newOrExistingUser[0].id
+      })}`
+    );
+  }
+}
