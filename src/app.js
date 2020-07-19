5b8cadb5e190ba0ebefc11fb9efa07de3d43b152
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-07-19 14:48:50 +0200

    chore(project setup)
    - add all folders and files
    - add basic eslint & bable configuration
    - add setup server to run
    - add source folder and all relevant folders

---
+++ src/app.js
@@ -0,0 +1,102 @@
+import http from 'http';
+import createError from 'http-errors';
+import express from 'express';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import session from 'express-session';
+import passport from 'passport';
+import socketIo from 'socket.io';
+import routes from './routes';
+import GamingRoomController from './controllers/GamingRoomController';
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+dotenv.config();
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY || 'ninjas-game',
+    cookie: { maxAge: 60000 },
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use('/api/v1/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err.status
+  });
+  next();
+});
+
+const gamingRoom = new GamingRoomController();
+io.on('connection', (socket) => {
+  socket.on('createRoom', (member, room) => {
+    const createdRoom = gamingRoom.createRoom(member, room);
+    if (!createdRoom.isRoom) {
+      socket.emit('createdRoom', createdRoom.newRoom);
+    }
+  });
+
+  socket.on('newMember', (member, room) => {
+    const joinedRoom = gamingRoom.joinRoom(member, room);
+    socket.emit('newMember', member, joinedRoom.room);
+    socket.broadcast.emit('newMember', member, joinedRoom.room);
+  });
+
+  socket.on('changeQuestion', (room) => {
+    const updatedRoom = gamingRoom.changeQuestion(room);
+    socket.emit('changeQuestion', updatedRoom);
+    socket.broadcast.emit('changeQuestion', updatedRoom);
+  });
+
+  socket.on('answerQuestion', (point, member, room) => {
+    const updatedRoom = gamingRoom.answerQuestion(point, member, room);
+    socket.emit('answerQuestion', point, member, updatedRoom);
+    socket.broadcast.emit('answerQuestion', point, member, updatedRoom);
+  });
+
+  socket.on('leaveGame', (member, room) => {
+    socket.broadcast.emit('leftGame', gamingRoom.leaveRoom(member, room));
+  });
+
+  socket.on('replayGame', (member, room) => {
+    const updatedRoom = gamingRoom.replayGame(member, room);
+    socket.emit('gameReplayed', updatedRoom);
+    socket.broadcast.emit('gameReplayed', updatedRoom);
+  });
+});
+
+app.server = server;
+export default app;
